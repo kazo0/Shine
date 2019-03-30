832b73353dfe0b2bd06291ef9347a8c0f7f3b0c9
Steve Bilogan <steve.bilogan@gmail.com>
2019-03-30 14:19:41 -0400

    Add step 0 based off of xamarin forms weather sample

--- Shine/Shine/MainPage.xaml.cs
+++ Shine/Shine/MainPage.xaml.cs
@@ -1,17 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shine.Models;
+using Shine.Services;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace Shine
 {
     public partial class MainPage : ContentPage
     {
+        private readonly OpenWeatherMapService _openWeatherMapService;
+
         public MainPage()
         {
             InitializeComponent();
+            _openWeatherMapService = new OpenWeatherMapService();
+        }
+
+        async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+            {
+                WeatherData weatherData =
+                    await _openWeatherMapService.GetWeatherData(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+                BindingContext = weatherData;
+            }
+        }
+
+        string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?q={_cityEntry.Text}";
+            requestUri += "&units=imperial"; // or units=metric
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
         }
     }
 }
